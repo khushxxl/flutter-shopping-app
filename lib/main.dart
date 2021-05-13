9cb4a057c1892480ebd2ae4f5890a2597ac6d20c
khushxxl <khushaal.choithramani@gmail.com>
2021-05-13 19:19:00 +0530

    commited lec 1

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(child: Text("FLUTTER REVISON"),),
+      ),
+      
+    );
+  }
+}
