2e1642f4d90e06daa5dae0bc8c9d12784c6c610b
khushxxl <khushaal.choithramani@gmail.com>
2021-05-15 13:08:52 +0530

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/screens/homepage.dart';
 import 'package:flutter_shopping_app/screens/login_page.dart';
 import 'package:flutter_shopping_app/utils/routes.dart';
+import 'package:flutter_shopping_app/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      initialRoute: MyRoutes.loginRoute,
+      theme: MyTheme.lightTheme(context),
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_app/widgets/myDrawer.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Shopping App"),
+        title: Text("Shopping App "),
       ),
       body: Center(
         child: Container(
-          child: Text("Flutter Revision"),
+          child: Text("Flutter Revision 2.0"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
